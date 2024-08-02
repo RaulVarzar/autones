426e8a16bd7aa70272161df83fa09acc0b7ce3c7
Raul Bogdan Varzar <raul_varzar@yahoo.com>
2024-08-02 14:59:27 +0300

    removed script

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-  title: 'Autones',
-  description: 'Tractari auto',
+  title: "Autones",
+  description: "Tractari auto",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script
+        {/* <script
           src="https://kit.fontawesome.com/d2dc86f0b2.js"
           crossorigin="anonymous"
-        ></script>
+        ></script> */}
       </head>
       <body className="bg-accent">{children}</body>
     </html>
